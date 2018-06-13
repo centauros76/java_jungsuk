8ab82e1d6d46bc568011a734cad5562dfec3a0e3
centauros <centauros@gmail.com>
2018-06-13 16:39:32 +0900

    첫 커밋

---
+++ src/com/tistory/centauros/day1/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.tistory.centauros.day1;
+
+/**
+ * Created by centauros on 2018-06-13.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
