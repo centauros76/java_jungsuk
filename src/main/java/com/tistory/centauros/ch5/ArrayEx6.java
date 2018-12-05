d713ba18f630b028945a32da3fb6db9fcfda5e4f
centauros <centauros@gmail.com>
2018-12-05 14:25:59 +0900

    ArrayEx6 Integer multipl

--- src/main/java/com/tistory/centauros/ch5/ArrayEx6.java
+++ src/main/java/com/tistory/centauros/ch5/ArrayEx6.java
@@ -6,6 +6,15 @@ package com.tistory.centauros.ch5;
  */
 public class ArrayEx6 {
     public static void main(String[] args) {
-        
+
+        Integer[] a = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        Integer[] b = a;
+
+        for (Integer x : a) {
+            for (Integer y : b) {
+                if (x.intValue() != 1)
+                    System.out.println(x.intValue() * y.intValue());
+            }
+        }
     }
 }
