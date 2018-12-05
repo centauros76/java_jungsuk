be0ef26084c93fdecf6d74776f7f2512fcd5093b
centauros <centauros@gmail.com>
2018-12-05 14:21:24 +0900

    ArrayEx6 editing

--- src/main/java/com/tistory/centauros/ch5/ArrayEx6.java
+++ src/main/java/com/tistory/centauros/ch5/ArrayEx6.java
@@ -1,11 +1,22 @@
 package com.tistory.centauros.ch5;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 /**
  * Created by Rene on 2018. 12. 5..
  * auth : Rene
  */
 public class ArrayEx6 {
     public static void main(String[] args) {
-        
+        Log log = LogFactory.getLog("studyLogger");
+
+        String[] arrStr = new String[5];
+        arrStr[0] = "A";
+        arrStr[1] = "B";
+        arrStr[2] = "C";
+        arrStr[3] = "D";
+        arrStr[4] = "E";
+
     }
 }
